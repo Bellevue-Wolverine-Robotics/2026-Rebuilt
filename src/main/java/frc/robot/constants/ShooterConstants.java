e040311d01ffe08958615f004fde1336014ec28c
Dylhouse <134633294+Dylhouse@users.noreply.github.com>
2026-02-07 12:43:18 -0800

    Add roller motor and flywheel motor PID control

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -1,7 +1,28 @@
 package frc.robot.constants;
 
 public class ShooterConstants {
-    // TODO: Assign real motor ids.
+    // TODO: Assign real values
     public static final int LEFT_MOTOR_ID = -1;
     public static final int RIGHT_MOTOR_ID = -1;
+
+    public static final int ROLLER_MOTOR_ID = -1;
+
+    public static final float ACCEPTABLE_RPS_TOLERANCE = -1;
+
+    // Voltage per motor rotation per second, or V/RPS. Found using SysId.
+    // Not the mechanical rps! Just the motor's!
+    public static final float VOLTAGE_PER_MOTOR_RPS = -1;
+
+    // Voltage to overcome the motor's static friction. Found using SysId
+    public static final float STATIC_FRICTION_OVERCOME_VOLTAGE = -1;
+
+    public static final float PROPORTIONAL_GAIN = -1;
+    public static final float INTEGRAL_GAIN = -1;
+    public static final float DERIVATIVE_GAIN = -1;
+
+    public static final float DIST_KX2 = 0;
+    public static final float DIST_KX1 = 1;
+    public static final float DIST_KX0 = 0;
+
+    public static final float ROLLER_VELOCITY = -1;
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,9 +2,13 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -12,29 +16,65 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ShooterConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
-    private final float ALLOWABLE_ZERO_DIST = 0.001f;
+    private final VelocityVoltage velocityRequest = new VelocityVoltage(-1.0d).withSlot(0);
+
+    private final TalonFX rollerMotor = new TalonFX(ShooterConstants.ROLLER_MOTOR_ID);
 
     private final TalonFX flywheelMasterMotor = new TalonFX(ShooterConstants.LEFT_MOTOR_ID);
     private final TalonFX flywheelFollowerMotor = new TalonFX(ShooterConstants.RIGHT_MOTOR_ID);
 
     public ShooterSubsystem() {
+        configureFlywheelMotors(flywheelMasterMotor, flywheelFollowerMotor);
+
+        var brakeConfig = new TalonFXConfiguration()
+            .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
+
+        rollerMotor.getConfigurator().apply(brakeConfig);
+    }
+
+    private static boolean withinTolerance(double value, double goal, double tolerance) {
+        return Math.abs(value - goal) < tolerance;
+    }
+
+    private static void configureFlywheelMotors(TalonFX master, TalonFX follower) {
+        Slot0Configs PIDConfig = new Slot0Configs();
+        PIDConfig.kS = ShooterConstants.STATIC_FRICTION_OVERCOME_VOLTAGE;
+        PIDConfig.kV = ShooterConstants.VOLTAGE_PER_MOTOR_RPS;
+        PIDConfig.kP = ShooterConstants.PROPORTIONAL_GAIN;
+        PIDConfig.kI = ShooterConstants.INTEGRAL_GAIN;
+        PIDConfig.kD = ShooterConstants.DERIVATIVE_GAIN;
+
+        master.getConfigurator().apply(PIDConfig);
+
         var invertedConfig = new TalonFXConfiguration();
         invertedConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
 
-        flywheelFollowerMotor.getConfigurator().apply(invertedConfig);
+        follower.getConfigurator().apply(invertedConfig);
     }
 
-    private double calculateFlywheelSpeed(double distance) {
-        // TODO: Add flywheel speed calculations
-        return -1.0f;
+    private void updateFlywheelMotorDesiredSpeed(double rotationsPerSecond) {
+        flywheelMasterMotor.setControl(velocityRequest.withVelocity(rotationsPerSecond));
     }
 
-    public Command shootCommand(DoubleSupplier distance, DoubleSupplier override) {
+    private double calculateFlywheelMotorSpeed(double distance) {
+        return 
+            // AX^2 + BX^1 + CX^0 or ax^2 + bx + c
+            ShooterConstants.DIST_KX2 * distance * distance +
+            ShooterConstants.DIST_KX1 * distance +
+            ShooterConstants.DIST_KX0;
+    }
+
+    public Command shootCommand(DoubleSupplier distance) {
         return new RunCommand(
-            () -> flywheelMasterMotor.set(
-                Math.abs(override.getAsDouble()) < ALLOWABLE_ZERO_DIST ? 
-                    calculateFlywheelSpeed(distance.getAsDouble()) :
-                    override.getAsDouble()),
+            () -> {
+                updateFlywheelMotorDesiredSpeed(calculateFlywheelMotorSpeed(distance.getAsDouble()));
+                if (withinTolerance(
+                        flywheelMasterMotor.getVelocity().getValueAsDouble(), 
+                        velocityRequest.Velocity, 
+                        ShooterConstants.ACCEPTABLE_RPS_TOLERANCE)) {
+                    rollerMotor.set(ShooterConstants.ROLLER_VELOCITY);
+                }
+            },
             this);
     }
 }
