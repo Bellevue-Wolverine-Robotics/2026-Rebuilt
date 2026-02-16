12c557158fa0d8056da39b4e2fcdd15463069624
Nathan Waxman-Jeng <54194229+nthnwj@users.noreply.github.com>
2026-02-16 15:33:20 -0800

    Add javadoc to ClimberSubsystem

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.constants.ClimberConstants;
 
+/* Represents the climber mechanism, allowing the robot to climb on the tower. */
 public class ClimberSubsystem extends SubsystemBase {
     private final DoubleSolenoid leftSolenoid = new DoubleSolenoid(
         PneumaticsModuleType.CTREPCM,
@@ -20,15 +21,30 @@ public class ClimberSubsystem extends SubsystemBase {
         ClimberConstants.RIGHT_REVERSE_CHANNEL
     );
 
+    /**
+     * Sets the valves on both sides to a certain posiiton.
+     * 
+     * @param valve The position of both valves.
+    */
     private void set(DoubleSolenoid.Value value) {
         leftSolenoid.set(value);
         rightSolenoid.set(value);
     }
 
+    /**
+     * Opens the forward side of the solenoid valves, to extend the intake arms.
+     * 
+     * @return Command that extends the arms.
+     */
     public Command extendCommand() {
         return runOnce(() -> set(DoubleSolenoid.Value.kForward));
     }
 
+    /**
+     * Opens the reverse side of the solenoid valves, to retract the arms.
+     * 
+     * @return Command that retracts the arms.
+     */
     public Command retractCommand() {
         return runOnce(() -> set(DoubleSolenoid.Value.kReverse));
     }
