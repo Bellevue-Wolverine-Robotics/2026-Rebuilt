f7e95ec6661592f02b398919864b83727323af66
Nathan Waxman-Jeng <54194229+nthnwj@users.noreply.github.com>
2026-02-18 14:13:24 -0800

    Use one double solenoid valve

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -9,27 +9,11 @@ import frc.robot.constants.ClimberConstants;
 
 /* Represents the climber mechanism, allowing the robot to climb on the tower. */
 public class ClimberSubsystem extends SubsystemBase {
-    private final DoubleSolenoid leftSolenoid = new DoubleSolenoid(
+    private final DoubleSolenoid solenoid = new DoubleSolenoid(
         PneumaticsModuleType.CTREPCM,
-        ClimberConstants.LEFT_FORWARD_CHANNEL,
-        ClimberConstants.LEFT_REVERSE_CHANNEL
+        ClimberConstants.FORWARD_CHANNEL,
+        ClimberConstants.REVERSE_CHANNEL
     );
-
-    private final DoubleSolenoid rightSolenoid = new DoubleSolenoid(
-        PneumaticsModuleType.CTREPCM,
-        ClimberConstants.RIGHT_FORWARD_CHANNEL,
-        ClimberConstants.RIGHT_REVERSE_CHANNEL
-    );
-
-    /**
-     * Sets the valves on both sides to a certain posiiton.
-     * 
-     * @param valve The position of both valves.
-    */
-    private void set(DoubleSolenoid.Value value) {
-        leftSolenoid.set(value);
-        rightSolenoid.set(value);
-    }
 
     /**
      * Opens the forward side of the solenoid valves, to extend the intake arms.
@@ -37,7 +21,7 @@ public class ClimberSubsystem extends SubsystemBase {
      * @return Command that extends the arms.
      */
     public Command extendCommand() {
-        return runOnce(() -> set(DoubleSolenoid.Value.kForward));
+        return runOnce(() -> solenoid.set(DoubleSolenoid.Value.kForward));
     }
 
     /**
@@ -46,6 +30,6 @@ public class ClimberSubsystem extends SubsystemBase {
      * @return Command that retracts the arms.
      */
     public Command retractCommand() {
-        return runOnce(() -> set(DoubleSolenoid.Value.kReverse));
+        return runOnce(() -> solenoid.set(DoubleSolenoid.Value.kReverse));
     }
 }
