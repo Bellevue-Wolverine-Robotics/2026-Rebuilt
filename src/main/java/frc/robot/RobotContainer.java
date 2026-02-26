6cf4aa24c0c4a0cd062629005e23a78a724df4df
thomas209 <112712071+thomas002009@users.noreply.github.com>
2026-02-26 00:25:57 -0800

    Write out initial autonomous routines (#26)
    Implements more advanced autonomous routes, with a drop down menu for the driver to choose from. Increases the current limit on swerve motors to 60 amps, as well as fixing the camera feed in the dashboard.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
@@ -25,8 +27,36 @@ public class RobotContainer {
     private final CommandXboxController driverController = new CommandXboxController(DriverStationConstants.DRIVER_CONTROLLER_PORT);
     private final CommandXboxController operatorController = new CommandXboxController(DriverStationConstants.OPERATOR_CONTROLLER_PORT);
 
+    private final SendableChooser<Command> sendableChooser = new SendableChooser<>();
+
     public RobotContainer() {
         configureBindings();
+        sendableChooser.setDefaultOption(
+                "Left One Cycle",
+                new PathPlannerAuto("ONE_CYCLE")
+        );
+        sendableChooser.addOption(
+                "Left Two Cycle",
+                new PathPlannerAuto("TWO_CYCLE")
+        );
+        sendableChooser.setDefaultOption(
+                "Right One Cycle",
+                new PathPlannerAuto("ONE_CYCLE", true)
+        );
+        sendableChooser.addOption(
+                "Right Two Cycle",
+                new PathPlannerAuto("TWO_CYCLE", true)
+        );
+        sendableChooser.addOption(
+                "Depot Left Tower",
+                new PathPlannerAuto("DEPOT_LEFT_TOWER")
+        );
+        sendableChooser.addOption(
+                "Depot Right Tower",
+                new PathPlannerAuto("DEPOT_RIGHT_TOWER")
+        );
+
+        SmartDashboard.putData("Auto Chooser", sendableChooser);
     }
 
     private void configureBindings() {
@@ -59,8 +89,8 @@ public class RobotContainer {
         driverController.a().whileTrue(swerveSubsystem.alignPoseCommand(VisionConstants.SHOOT_POSE_SUPPLIER));
         driverController.b().whileTrue(swerveSubsystem.alignPoseCommand(VisionConstants.CLIMB_POSE_SUPPLIER));
     }
-
     public Command getAutonomousCommand() {
-        return new PathPlannerAuto("Basic");
+        return sendableChooser.getSelected();
     }
+
 }
