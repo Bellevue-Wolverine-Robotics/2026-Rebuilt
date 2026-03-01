0343022539da7829531c09c7551c91ce8ab2ac77
Nathan Waxman-Jeng <54194229+nthnwj@users.noreply.github.com>
2026-03-01 10:35:54 -0800

    Remove unused imports

--- src/main/java/frc/robot/commands/AutoShootCommand.java
+++ src/main/java/frc/robot/commands/AutoShootCommand.java
@@ -6,7 +6,6 @@ import java.util.function.Supplier;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.constants.AlignmentConstants;
