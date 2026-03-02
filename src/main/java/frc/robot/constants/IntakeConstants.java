d497b8a49eac4470965ec24348dca5c9fb1d9f40
Nathan Waxman-Jeng <54194229+nthnwj@users.noreply.github.com>
2026-03-02 12:00:08 -0800

    Remove extra line from end of IntakeConstants

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -5,4 +5,3 @@ public class IntakeConstants {
     public static final double INTAKE_SPEED = 1.0; // TOOD: Replace with actual intake speed
     public static final double UNJAM_SPEED = -1.0; // TODO: Replace with actual unjam speed
 }
-
